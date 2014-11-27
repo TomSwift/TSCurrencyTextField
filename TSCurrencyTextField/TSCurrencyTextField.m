ad39b262aea18d0e6207344cd97c2713265ed59e
rtoshiro <rtoshiro@gmail.com>
2014-11-27 18:38:29 -0200

    - Added: maximumLength property

--- TSCurrencyTextField/TSCurrencyTextField.m
+++ TSCurrencyTextField/TSCurrencyTextField.m
@@ -43,6 +43,7 @@
 
 - (void) TSCurrencyTextField_commonInit
 {
+    _maximumLength = 0;
     _invalidInputCharacterSet = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
 
     _currencyNumberFormatter = [[NSNumberFormatter alloc] init];
@@ -155,6 +156,9 @@
     int distanceFromEnd = textField.text.length - (range.location + range.length);
     
     NSString* changed = [textField.text stringByReplacingCharactersInRange: range withString: string];
+    if (textField.maximumLength > 0 && changed.length > textField.maximumLength)
+      return NO;
+  
     if ([self.delegate respondsToSelector:@selector(textField:shouldChangeCharactersInRange:replacementString:)])
     {
       if ([self.delegate textField:textField shouldChangeCharactersInRange:range replacementString:string])
